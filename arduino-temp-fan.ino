89fddc130f485ac085f08f4826cd71cc3c6873e8
Matt Shinall <matt.shinall@gmail.com>
2019-06-29 11:48:13 -0400

    initial checkin

---
+++ arduino-temp-fan.ino
@@ -0,0 +1,45 @@
+const int A_BTM = 30;
+const int A_TOP = 300;
+const int I_BTM = 0;
+const int I_TOP = 5;
+const int MODE_CNT = 6;
+const int P_MODES[] = {0, 50, 100, 150, 200, 255};
+const int L_PINS[]  = {0,  8,   9,  10,  11,  12};
+const int A_IN_PIN = A0;
+const int P_OUT_PIN = 3;
+const int FAN_DELAY = 1000;
+
+void setup() {
+    //Serial.begin(9600);
+    pinMode(A_IN_PIN, INPUT);
+    pinMode(P_OUT_PIN, OUTPUT);
+    for(int i = 0; i < MODE_CNT; i++) {
+      pinMode(L_PINS[i], OUTPUT);
+    }
+    ledsOff();
+}
+
+void loop() {
+  int aVal = analogRead(A_IN_PIN);
+  int iVal = map(aVal, A_BTM, A_TOP, I_BTM, I_TOP);
+  iVal = (iVal > I_TOP) ? I_TOP : iVal;
+  iVal = (iVal < I_BTM) ? I_BTM : iVal;
+  int pVal = P_MODES[iVal];
+  int lPin = L_PINS[iVal];
+  analogWrite(P_OUT_PIN, pVal);
+  ledsOff();
+  digitalWrite(lPin, HIGH);
+  /*
+  Serial.print("aVal=");
+  Serial.print(aVal);
+  Serial.print(", pVal=");
+  Serial.println(pVal);
+  */
+  delay(FAN_DELAY);
+}
+
+void ledsOff() {
+  for(int i = 0; i < MODE_CNT; i++) {
+    digitalWrite(L_PINS[i], LOW);
+  }
+}
