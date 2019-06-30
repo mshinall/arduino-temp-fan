a3c27ca8cbe6b50e7c82ed71c01b7f31402369a0
mshinall <matt.shinall@gmail.com>
2019-06-30 19:13:42 +0000

    updated

--- arduino-temp-fan.ino
+++ arduino-temp-fan.ino
@@ -3,7 +3,7 @@ const int A_TOP = 1000;
 const int I_BTM = 0;
 const int I_TOP = 5;
 const int MODE_CNT = 6;
-const int P_MODES[] = {0, 150, 170, 190, 220, 255};
+const int P_MODES[] = {0, 50, 100, 150, 200, 255};
 const int L_PINS[]  = {0,  8,   9,  10,  11,  12};
 const int A_IN_PIN = A5;
 const int P_OUT_PIN = 3;
@@ -12,7 +12,7 @@ const int FAN_DELAY = 1000;
 int curFanSpeed = 0;
 
 void setup() {
-    Serial.begin(9600);
+    //Serial.begin(9600);
     pinMode(A_IN_PIN, INPUT);
     pinMode(P_OUT_PIN, OUTPUT);
     for(int i = 0; i < MODE_CNT; i++) {
