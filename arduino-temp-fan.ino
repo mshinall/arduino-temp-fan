502f71ea12383f1d0e054ba2da9005bb85ba3708
mshinall <matt.shinall@gmail.com>
2019-06-30 14:18:10 +0000

    updated

--- arduino-temp-fan.ino
+++ arduino-temp-fan.ino
@@ -3,11 +3,13 @@ const int A_TOP = 1000;
 const int I_BTM = 0;
 const int I_TOP = 5;
 const int MODE_CNT = 6;
-const int P_MODES[] = {0, 50, 100, 150, 200, 255};
+const int P_MODES[] = {0, 150, 170, 190, 220, 255};
 const int L_PINS[]  = {0,  8,   9,  10,  11,  12};
 const int A_IN_PIN = A5;
 const int P_OUT_PIN = 3;
 const int FAN_DELAY = 1000;
+
+int curFanSpeed = 0;
 
 void setup() {
     Serial.begin(9600);
@@ -26,10 +28,12 @@ void loop() {
   iVal = (iVal < I_BTM) ? I_BTM : iVal;
   int pVal = P_MODES[iVal];
   int lPin = L_PINS[iVal];
-  analogWrite(P_OUT_PIN, pVal);
-  ledsOff();
-  digitalWrite(lPin, HIGH);
-  /**/
+  if(pVal != curFanSpeed) {
+    setFanSpeed(pVal);
+    curFanSpeed = pVal;
+    setLedMode(lPin);
+  }
+  /*
   Serial.print("aVal=");
   Serial.print(aVal);
   Serial.print(", pVal=");
@@ -38,8 +42,19 @@ void loop() {
   Serial.print(iVal);
   Serial.print(", lPin=");
   Serial.println(lPin);
-  /**/
+  */
   delay(FAN_DELAY);
+}
+
+void setLedMode(int lPin){
+  ledsOff();
+  digitalWrite(lPin, HIGH); 
+}
+
+void setFanSpeed(int pVal) {
+  analogWrite(P_OUT_PIN, 255);
+  delay(500);
+  analogWrite(P_OUT_PIN, pVal);
 }
 
 void ledsOff() {
